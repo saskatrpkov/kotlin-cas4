239bfa4b963a50281e4b55e0238fa9656a425d8c
saskatrpkov <aleksandratrpkov1@gmail.com>
2024-11-28 11:46:37 +0100

    odradjeni spash,signin,signup i home fragmenti

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -7,35 +7,12 @@ import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    val TAG="Activity 1"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG,"onCreate")
-        var btn_next=findViewById<Button>(R.id.btnNext)
-        btn_next.setOnClickListener {
-            var intent=Intent(this,MainActivity2::class.java)
-            startActivity(intent)
-        }
+
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG,"onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG,"onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG,"onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG,"onStop")
-    }
 }
